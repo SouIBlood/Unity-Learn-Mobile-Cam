370efb74ab3b9206cf613ec4f1d610f437467f26
Yong Xiang <yong.xiang97@gmail.com>
2021-10-14 22:59:06 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mobile_Cam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
