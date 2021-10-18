a09866e8052870bff5e60caaa304015e29727f3f
Yong Xiang <yong.xiang97@gmail.com>
2021-10-19 02:33:28 +0800

    server

--- Server/index.py
+++ Server/index.py
@@ -9,13 +9,16 @@ app = Flask(__name__)
 
 @app.route('/upload', methods=["POST"])
 def do_upload():
-
     upload     = request.files.get('image')
     name, ext = os.path.splitext(upload.filename)
 
+    # upload.save('D:\\python\\test.jpeg')
+
+    # upload = open('D:\\python\\test.jpeg', 'rb')
+    
     return Response(
         response = upload.read(),
-        mimetype = 'image/*'
+        mimetype = 'image/jpeg'
     )
 
 app.run(host='0.0.0.0')
