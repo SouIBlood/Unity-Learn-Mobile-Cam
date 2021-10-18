fa72630afc12dfca6aa4f243f451d4f51439e92c
SouIBlood <yong.xiang97@gmail.com>
2021-10-18 09:06:44 +0800

    remove plugin

--- Assets/Scenes/PhoneCamera.cs
+++ Assets/Scenes/PhoneCamera.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using OpenCvSharp;
 
 public class PhoneCamera: MonoBehaviour
 {
@@ -14,18 +13,18 @@ public class PhoneCamera: MonoBehaviour
     public AspectRatioFitter fitter;
 
 
-
+    private Texture2D outTexture;
 
     // Start is called before the first frame update
     void Start()
     {
-        defaultBackground = background.texture;
+        this.defaultBackground = background.texture;
         WebCamDevice[] devices = WebCamTexture.devices;
 
         if(devices.Length == 0)
         {
             Debug.Log("No camera detected");
-            camAvailable = false;
+            this.camAvailable = false;
             return;
         }
         
@@ -33,52 +32,46 @@ public class PhoneCamera: MonoBehaviour
         {
             if(!devices[i].isFrontFacing)
             {
-                backCam = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
+                this.backCam = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
             }
         }
 
-        if(backCam == null)
+        if(this.backCam == null)
         {
             Debug.Log("No back camera detected");
-            camAvailable = false;
+            this.camAvailable = false;
             return;
         }
 
-        backCam.Play();
-        // background.texture = backCam;
+        this.backCam.Play();
 
-        camAvailable = true;
+        // this.background.texture = this.backCam;
+        
+
+        // Apply Camera Ratio to background
+        float ratio = (float)backCam.width / (float)backCam.height;
+        this.fitter.aspectRatio = ratio;
+
+        float scaleY = backCam.videoVerticallyMirrored ? -1f: 1f;
+        this.background.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
+
+        int orient = -backCam.videoRotationAngle;
+        this.background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
+
+        this.camAvailable = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!camAvailable)
+        if(!this.camAvailable)
         {
             return;
         }
 
-        GetComponent<Renderer>().material.mainTexture = backCam;
-        Mat frame = OpenCvSharp.Unity.TextureToMat(backCam);
-        
-        
-        FrameToHSV(frame);
-        // float ratio = (float)backCam.width / (float)backCam.height;
-        // fitter.aspectRatio = ratio;
 
-        // float scaleY = backCam.videoVerticallyMirrored ? -1f: 1f;
-        // background.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
 
-        // int orient = -backCam.videoRotationAngle;
-        // background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
     }
 
-    private void FrameToHSV(Mat frame)
-    {
-        Mat greyFrame = new Mat();
 
-        Cv2.CvtColor(frame, greyFrame, ColorConversionCodes.RGB2GRAY);
-
-        background.texture = OpenCvSharp.Unity.MatToTexture(greyFrame);
-    }
 }
